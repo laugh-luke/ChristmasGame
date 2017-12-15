8f093a33ba477c59f332c9a8ea057d92b88390d6
KSscott78 <Kieran.Scott@qa.com>
2017-12-15 08:45:30 +0000

    canvas creation

---
+++ script.js
@@ -0,0 +1,6 @@
+// Create the canvas
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 600;
+canvas.height = 600;
+document.body.appendChild(canvas);
